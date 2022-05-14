77ecd8c7a7718f8a004d6a43eaae74dc3e89a467
moh0009 <83363934+moh0009@users.noreply.github.com>
2022-05-14 16:24:41 +0000

    clean code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #import librarys that we well use
 import telebot
-import requests
+import requests as re
 import os
 #token
 g = os.environ['g']
@@ -18,47 +18,56 @@ and i well send to you a page photo and audio""")
 #when user send page number
 @bot.message_handler()
 def page_num(message):
-    
-    txt = message.text
-    
-    #if message value >= 1 and <=604
-    if int(txt) >= 1 and int(txt) <= 604:
-        image_name = f'{txt}.png'
-        img_data = requests.get(f"https://quran.ksu.edu.sa/tajweed_png/{txt}.png").content
-        with open(image_name, 'wb') as handler:
-            handler.write(img_data)
-            
-        #if message value >= 1 and <=9
-        if int(txt) >= 1 and int(txt)<=9:
-            mp3_name = f'{txt}.mp3'
-            mp3_data = requests.get(f"https://ia801803.us.archive.org/10/items/1-mp-3-128-k/ألقران الكريم - أيمن سويد الصفحة 00{txt}(MP3_128K).mp3").content
-            with open(mp3_name, 'wb') as handler:
-                handler.write(mp3_data)
-
-        #if message value >= 10 and <=99        
-        if int(txt) >= 10 and int(txt)<=99:
-            mp3_name = f'{txt}.mp3'
-            mp3_data = requests.get("https://ia801803.us.archive.org/10/items/1-mp-3-128-k/ألقران الكريم - أيمن سويد الصفحة 0"+txt+"(MP3_128K).mp3" ).content
-            with open(mp3_name, 'wb') as handler:
-                handler.write(mp3_data)
-                
-        #if message value >= 100 and <=604       
-        if int(txt) >= 100 and int(txt)<=604:
-            mp3_name = f'{txt}.mp3'
-            mp3_data = "https://ia801803.us.archive.org/10/items/1-mp-3-128-k/ألقران الكريم - أيمن سويد الصفحة "+txt+"(MP3_128K).mp3" 
-            with open(mp3_name, 'wb') as handler:
-                handler.write(mp3_data)
-                
-        #sending page photo and audio
-        bot.send_photo(message.chat.id,open(image_name,"rb") , caption=f"page number : {txt}")
-        bot.send_audio(message.chat.id,open(mp3_name,"rb") , caption=f"page number : {txt}")
+    text = message.text
+    id = message.chat.id
+    if text.isdigit() == True:
         
-        #removeing page photo and audio from storage 
-        os.remove(image_name)
-        os.remove(mp3_name)
+        if int(text) <= 604 and int(text)>=1:
+            
+            img_name = f"{text}.png"
+            img_data = re.get(f"https://image.slidesharecdn.com/random-160202103353/95/-{text}-1024.jpg").content
 
-    #if value is not true
+            with open(img_name, 'wb') as handler:
+                handler.write(img_data) 
+
+            bot.send_photo(id,open(img_name,"rb") , caption=f"page number : {text}")
+            os.remove(img_name)
+
+        if int(text) <= 9 and int(text)>=1:
+            
+            mp3_name = f"{text}.mp3"
+            mp3_data = re.get(f"https://ia801803.us.archive.org/10/items/1-mp-3-128-k/ألقران الكريم - أيمن سويد الصفحة 00{text}(MP3_128K).mp3").content
+
+            with open(mp3_name, 'wb') as handler:
+                handler.write(mp3_data) 
+
+            bot.send_audio(id,open(mp3_name,"rb") , caption=f"page number : {text}")
+            os.remove(mp3_name)
+
+        if int(text) <= 99 and int(text)>=10:
+            
+            mp3_name = f"{text}.mp3"
+            mp3_data = re.get(f"https://ia801803.us.archive.org/10/items/1-mp-3-128-k/ألقران الكريم - أيمن سويد الصفحة 0{text}(MP3_128K).mp3").content
+
+            with open(mp3_name, 'wb') as handler:
+                handler.write(mp3_data) 
+
+            bot.send_audio(id,open(mp3_name,"rb") , caption=f"page number : {text}")
+            os.remove(mp3_name)
+
+        if int(text) <= 604 and int(text)>=100:
+            
+            mp3_name = f"{text}.mp3"
+            mp3_data = re.get(f"https://ia801803.us.archive.org/10/items/1-mp-3-128-k/ألقران الكريم - أيمن سويد الصفحة {text}(MP3_128K).mp3").content
+
+            with open(mp3_name, 'wb') as handler:
+                handler.write(mp3_data) 
+
+            bot.send_audio(id,open(mp3_name,"rb") , caption=f"page number : {text}")
+            os.remove(mp3_name)
+
+        else:
+            bot.send_message(id,"wrong page number")
     else:
-        str(txt)
-        bot.send_message(message.chat.id , "wrong page number")
+        bot.send_message(id,"wrong page type")
 bot.polling()
